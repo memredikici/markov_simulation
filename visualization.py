5e519cf0f2283605d828c8a331e0eb37805e037d
Mehmet Emre Dikici <emredikici26@gmail.com>
2021-06-11 15:51:45 +0200

    update Visualization

--- visualization.py
+++ visualization.py
@@ -1,9 +1,9 @@
 import numpy as np
+import pandas as pd
 import cv2
 import random
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
-#from classes import Supermarket
 
 from pandas.io.parsers import read_csv
 
@@ -29,7 +29,7 @@ MARKET = """
 class SupermarketMap:
     """Visualizes the supermarket background"""
 
-    def __init__(self, layout, tiles,opening,closing):
+    def __init__(self, layout, tiles, opening, closing):
         """
         layout : a string with each character representing a tile
         tile   : a numpy array containing the tile image
@@ -46,33 +46,33 @@ class SupermarketMap:
         self.closing = 0
 
     def extract_tile(self, row, col):
-        y = (row-1)*32
-        x = (col-1)*32
-        return self.tiles[y:y+32, x:x+32]
+        y = (row - 1) * 32
+        x = (col - 1) * 32
+        return self.tiles[y:y + 32, x:x + 32]
 
     def get_tile(self, char):
         """returns the array for a given tile character"""
         if char == "#":
             return self.extract_tile(1, 1)
-            #return self.tiles[0:32, 0:32]
+            # return self.tiles[0:32, 0:32]
         elif char == "G":
             return self.extract_tile(8, 4)
-            #return self.tiles[7 * 32: 8 * 32, 3 * 32: 4 * 32]
+            # return self.tiles[7 * 32: 8 * 32, 3 * 32: 4 * 32]
         elif char == "C":
             return self.extract_tile(3, 9)
-            #return self.tiles[2 * 32: 3 * 32, 8 * 32: 9 * 32]
+            # return self.tiles[2 * 32: 3 * 32, 8 * 32: 9 * 32]
         elif char == "b":
             return self.extract_tile(1, 5)
-            #return self.tiles[0: 32, 4 * 32: 5 * 32]
+            # return self.tiles[0: 32, 4 * 32: 5 * 32]
         elif char == "s":
             return self.extract_tile(5, 10)
-        elif char == "d": #dairy
+        elif char == "d":  # dairy
             return self.extract_tile(6, 7)
-        elif char == "D": #drinks
+        elif char == "D":  # drinks
             return self.extract_tile(7, 14)
         else:
             return self.extract_tile(2, 3)
-            #return self.tiles[32:64, 64:96]
+            # return self.tiles[32:64, 64:96]
 
     def prepare_map(self):
         """prepares the entire image as a big numpy array"""
@@ -106,37 +106,35 @@ class Customer:
         self.location = 0
         self.image = image
         self.x = 0
-        self.y = 0          
-        
+        self.y = 0
 
     def __repr__(self):
         return f'Customer'
 
     def draw(self, frame, location):
         if location == 'dairy':
-            self.x = random.choice([6,7]) 
-            self.y = random.choice([2,6])# 2,6
-            
+            self.x = random.choice([6, 7])
+            self.y = random.choice([2, 3, 4, 5, 6])
+
         elif location == 'drinks':
-            self.x = random.choice([2,3])# 2,3
-            self.y = random.choice([2,6])# 2,6
-            print(self.x,self.y)
-            
+            self.x = random.choice([2, 3])
+            self.y = random.choice([2, 3, 4, 5, 6])
+            print(self.x, self.y)
+
         elif location == 'fruit':
-            self.x = random.choice([14,15])# 14,15
-            self.y = random.choice([2,6])# 2,6
+            self.x = random.choice([14, 15])
+            self.y = random.choice([2, 3, 4, 5, 6])
         elif location == 'spices':
-            self.x = random.choice([10,11])# 10,11
-            self.y = random.choice([2,6])# 2,6
+            self.x = random.choice([10, 11])
+            self.y = random.choice([2, 3, 4, 5, 6])
         elif location == 'checkout':
-            self.x = 5# 10,11
-            #self.x = random.choice([4,5])# 10,11
-            self.y = random.choice([8,10])# 2,6 
-        print(self.x,self.y, location)
+            self.x = random.choice([5, 9, 13])
+            self.y = random.choice([8, 9])
+        print(self.x, self.y, location)
         xpos = OFS + self.x * TILE_SIZE
         ypos = OFS + self.y * TILE_SIZE
-        #frame[ypos: ypos+32, xpos: xpos+ 32] = self.image
-        frame[ypos: ypos+self.image.shape[0], xpos: xpos+ self.image.shape[1]] = self.image
+        frame[ypos: ypos + self.image.shape[0],
+              xpos: xpos + self.image.shape[1]] = self.image
         # overlay the Customer image / sprite onto the frame
 
     def move(self, direction):
@@ -154,55 +152,48 @@ if __name__ == "__main__":
 
     background = np.zeros((484, 676, 3), np.uint8)
     tiles = cv2.imread("tiles.png")
-    simulated_table = read_csv('./data/Simulated_Market_Table/simulated_market_table_average.csv')
-    print(simulated_table)
-    opening = simulated_table['time'].iloc[0]
-    closing = simulated_table['time'].iloc[-1]
-    
-    section = simulated_table.loc[simulated_table['customer_no']==1]['location']
-    print(type(section),section)
+    simulated_table = read_csv('./data/Simulated_Market_Table/simulated_market_table_average.csv',
+                               parse_dates=['timestamp'])
 
-    def extract(row, col,tiles):
-        y = (row-1)*32
-        x = (col-1)*32
-        return tiles[y:y+32, x:x+32]
-    
-    customer_figure = extract(8,2,tiles)
-    market = SupermarketMap(MARKET, tiles,opening,closing)
-    customer = Customer(market,customer_figure)
-    #customer2 = Customer(market,customer_figure,15,8)
-    minute = simulated_table['time'].loc[simulated_table['time']=='07:00:00']
-    print('before',type(minute))
-    #minute = datetime.strptime(minute, '%H:%M:%S')
-    print('after',type(minute))
-    #opening = datetime.strptime(opening, '%H:%M:%S')
-    #closing = datetime.strptime(closing, '%H:%M:%S')
-    while (opening <= minute) and  (minute <= closing):
-        
+    opening = simulated_table['timestamp'].iloc[0]
+    closing = simulated_table['timestamp'].iloc[-1]
+    section = simulated_table.loc[simulated_table['customer_no']
+                                  == 1]['location']
+
+    def extract(row, col, tiles):
+        y = (row - 1) * 32
+        x = (col - 1) * 32
+        return tiles[y:y + 32, x:x + 32]
+    customer_figure = cv2.imread("./data/figures/figure1.png")
+
+    #customer_figure = extract(8, 2, tiles)
+    market = SupermarketMap(MARKET, tiles, opening, closing)
+    customer = Customer(market, customer_figure)
+
+    for i in range(0, len(simulated_table['timestamp'])):
+
+        time_current = opening
         frame = background.copy()
         market.draw(frame)
-        
-        for customer_index in simulated_table.loc[simulated_table['timestamp']==minute]['customer_no']:
-            section = simulated_table.loc[simulated_table['customer_no']==customer_index]['location']
-            for i in section:         
-                
-                print(i)
-                customer.draw(frame,i)
-                cv2.imshow("frame", frame)
-                time.sleep(1)
+        for customer_index in simulated_table.loc[simulated_table['timestamp'] == time_current]['customer_no']:
 
-            
-        #customer2.draw(frame)
-        
-        key = chr(cv2.waitKey(1) & 0xFF)
-        
-        if key == "q":
-            break
-        if key == "w":    
-            customer.move('up')
-            #customer2.move('up')
-        minute = minute + datetime.timedelta(minutes = 1)
+            print('time:', time_current, 'customer_no:', customer_index)
+            section = 0
+            section = pd.DataFrame(simulated_table.loc[(simulated_table['customer_no'] == customer_index) & (
+                simulated_table['timestamp'] == time_current)])
+            print(section)
+            section = str(section['location']).split()[1]
+            print(section)
+
+            customer.draw(frame, section)
+            cv2.imshow("frame", frame)
+            time.sleep(0.8)
+            key = chr(cv2.waitKey(1) & 0xFF)
+
+            if key == "q":
+                break
+            if key == "w":
+                customer.move('up')
+        opening = opening + timedelta(minutes=1)
 
     cv2.destroyAllWindows()
-
-    market.write_image("supermarket.png")
